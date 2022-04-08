f492a1ad3bccd2a60cd0994bacdea91aead6518b
Sunny996 <satlaakash@gmail.com>
2022-04-09 01:44:39 +0530

    ADDED: A test case to delete from queue using linked list

--- src/main/MyStackTest.java
+++ src/main/MyStackTest.java
@@ -14,7 +14,19 @@ public class MyStackTest {
         myStack.enQueue(mySecondNode);
         myStack.enQueue(myThirdNode);
         INode<Integer> myNode=myStack.peak();
-        myStack.printStack();
+        System.out.println(myNode.getKey());
+        Assert.assertEquals(myFirstNode,myNode);
+    }
+    @Test
+    public void given3NumbersWhenDeletedFromQueueShouldMatchFirstAddedNode() {
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.enQueue(myFirstNode);
+        myStack.enQueue(mySecondNode);
+        myStack.enQueue(myThirdNode);
+        INode<Integer> myNode=myStack.deQueue();
         Assert.assertEquals(myFirstNode,myNode);
     }
 }

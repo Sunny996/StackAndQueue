0e084fb7a349234e0c769e621a8f37c98fc7b257
Sunny996 <satlaakash@gmail.com>
2022-04-09 00:59:15 +0530

    ADDED: A test case to delete element from stack

--- src/main/MyStackTest.java
+++ src/main/MyStackTest.java
@@ -5,6 +5,19 @@ import org.junit.Test;
 
 public class MyStackTest {
     @Test
+    public void given3NumbersInStackWhenPoppedShouldMatchWithLastAddedNode() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode<Integer> poppedNode=myStack.pop();
+        myStack.printStack();
+        Assert.assertEquals(myThirdNode,poppedNode);
+    }
+    @Test
     public void given3NumbersWhenAddedToStackShouldHaveLastAddedNode() {
         MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);

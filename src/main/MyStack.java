fb84e425416ba45f405e2a0911fc4006fe93af10
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-09 09:47:05 +0530

    Update MyStack.java

--- src/main/MyStack.java
+++ src/main/MyStack.java
@@ -1,27 +1,27 @@
-	package main;
+package main;
 
-	public class MyStack<K> {
-		private final MyLinkedList<K> myLinkedList;
+public class MyStack<K> {
+    private final MyLinkedList<K> myLinkedList;
 
-		public MyStack() {
-			this.myLinkedList = new MyLinkedList();
-		}
+    public MyStack() {
+        this.myLinkedList = new MyLinkedList();
+    }
 
-		public void push(INode element) {
-			myLinkedList.addNode(element);
-		}
+    public void push(INode element) {
+        myLinkedList.addNode(element);
+    }
 
-		public INode<K> peak() {
-			return myLinkedList.getHead();
-		}
+    public INode<K> peak() {
+        return myLinkedList.getHead();
+    }
 
-		public INode<K> pop() {
-			return myLinkedList.pop();
-		}
+    public INode<K> pop() {
+        return myLinkedList.pop();
+    }
 
-		public void printStack() {
-			myLinkedList.print();
-		}
-	}
+    public void printStack() {
+        myLinkedList.print();
+    }
+}
 
 

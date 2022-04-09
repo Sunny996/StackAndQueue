3b2dceaec79b41b8f034a6cae1fe1267a6fa6b75
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-09 09:40:21 +0530

    Update MyQueueTest.java

--- src/main/MyQueueTest.java
+++ src/main/MyQueueTest.java
@@ -4,36 +4,30 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MyQueueTest {
-    @Test
+       @Test
     public void given3NumbersWhenAddedToQueueShouldHaveFirstAddedNode() {
         MyNode<Integer> myThirdNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyQueue myQueue = new MyQueue();
-        myQueue.enQueue(myFirstNode);
-        myQueue.enQueue(mySecondNode);
-        myQueue.enQueue(myThirdNode);
-        INode<Integer> myNode = myQueue.peak();
-<<<<<<< HEAD
-        Assert.assertEquals(myFirstNode, myNode);
-    }
-}
-=======
+        MyStack myStack = new MyStack();
+        myStack.enQueue(myFirstNode);
+        myStack.enQueue(mySecondNode);
+        myStack.enQueue(myThirdNode);
+        INode<Integer> myNode=myStack.peak();
         System.out.println(myNode.getKey());
-        Assert.assertEquals(myFirstNode, myNode);
+        Assert.assertEquals(myFirstNode,myNode);
     }
-
     @Test
     public void given3NumbersWhenDeletedFromQueueShouldMatchFirstAddedNode() {
         MyNode<Integer> myThirdNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
         MyNode<Integer> myFirstNode = new MyNode<>(56);
-        MyQueue myQueue = new MyQueue();
-        myQueue.enQueue(myFirstNode);
-        myQueue.enQueue(mySecondNode);
-        myQueue.enQueue(myThirdNode);
-        INode<Integer> myNode = myQueue.deQueue();
-        Assert.assertEquals(myFirstNode, myNode);
+        MyStack myStack = new MyStack();
+        myStack.enQueue(myFirstNode);
+        myStack.enQueue(mySecondNode);
+        myStack.enQueue(myThirdNode);
+        INode<Integer> myNode=myStack.deQueue();
+        Assert.assertEquals(myFirstNode,myNode);
     }
 }
->>>>>>> UC4_DeletedElementsFromQueueUsingPop
+

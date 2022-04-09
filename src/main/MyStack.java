8bd3260668d096dc0ddf6f075f0d43e644a0dd41
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-04-09 09:39:18 +0530

    Update MyStack.java

--- src/main/MyStack.java
+++ src/main/MyStack.java
@@ -1,53 +1,27 @@
-package main;
+	package main;
 
-public class MyQueue<K> {
-    private final MyLinkedList<K> myLinkedList;
+	public class MyStack<K> {
+		private final MyLinkedList<K> myLinkedList;
 
-    public MyStack() {
-        this.myLinkedList = new MyLinkedList();
-    }
+		public MyStack() {
+			this.myLinkedList = new MyLinkedList();
+		}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public void enQueue(INode element) {
-        myLinkedList.appendNode(element);
-    }
+		public void push(INode element) {
+			myLinkedList.addNode(element);
+		}
 
-    public INode<K> deQueue() {
-        return myLinkedList.pop();
-    }
+		public INode<K> peak() {
+			return myLinkedList.getHead();
+		}
 
-    public boolean isEmpty() {
-        return myLinkedList.size() == 0;
-    }
+		public INode<K> pop() {
+			return myLinkedList.pop();
+		}
 
-    public int size() {
-        return myLinkedList.size();
-    }
-
-    public void printStack() {
-        myLinkedList.print();
-=======
-    public void push(INode element) {
-        myLinkedList.addNode(element);
->>>>>>> UC3_CreatedQueueAndAddedElementsUsingAppend
-=======
-    public void push(INode element) {
-        myLinkedList.addNode(element);
->>>>>>> UC4_DeletedElementsFromQueueUsingPop
-    }
-
-    public INode<K> peak() {
-        return myLinkedList.getHead();
-    }
-
-    public INode<K> pop() {
-        return myLinkedList.pop();
-    }
-
-    public void printStack() {
-        myLinkedList.print();
-    }
-}
+		public void printStack() {
+			myLinkedList.print();
+		}
+	}
 
 
